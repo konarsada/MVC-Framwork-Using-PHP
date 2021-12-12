0415b66befd8d6bc0d7acb0ba67434540bb526ac
konarsada <sadabratak@gmail.com>
2021-12-13 00:54:38 +0530

    validate data in server

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -25,7 +25,12 @@ class Signup extends \Core\Controller {
      */
     public function createAction() {
         $user = new User($_POST);
-        $user->save();
-        View::renderTemplate('Signup/success.html');
+
+        if($user->save()) {
+            View::renderTemplate('Signup/success.html');
+        }
+        else {
+            var_dump($user->errors);
+        }
     }
 }
--- App/Models/User.php
+++ App/Models/User.php
@@ -9,6 +9,13 @@ use PDO;
  */
 class User extends \Core\Model {
     /**
+     * Error messages
+     * 
+     * @var array
+     */
+    public $errors = [];
+
+    /**
      * Class constructor
      * 
      * @param array $data Initial property values
@@ -24,21 +31,62 @@ class User extends \Core\Model {
     /**
      * Save the user model with the current property values
      * 
-     * @return void
+     * @return boolean True if the user was saved, false otherwise
      */
     public function save() {
-        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
-        
-        $sql = 'INSERT INTO users (name, email, password_hash)
-            VALUES (:name, :email, :password_hash)';
-        
-        $db = static::getDB();
-        $stmt = $db->prepare($sql);
+        $this->validate();
 
-        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+        if(empty($this->errors)) {
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+            
+            $sql = 'INSERT INTO users (name, email, password_hash)
+                VALUES (:name, :email, :password_hash)';
+            
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
 
-        $stmt->execute();
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+
+        return false;
     }
+
+    /**
+     * Validate current property values, adding validation error messages to the errors array property
+     * 
+     * @return void
+     */
+    public function validate() {
+        // Name
+        if($this->name == '') {
+            $this->errors[] = 'Name is required';
+        }
+
+        // Email address
+        if(filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
+            $this->errors[] = 'Invalid email';
+        }
+
+        // Password
+        if($this->password != $this->password_confirmation) {
+            $this->errors[] = 'Password must match confirmation';
+        }
+
+        if(strlen($this->password) < 6) {
+            $this->errors[] = 'Please enter atleast 6 characters for the password';
+        }
+
+        if(preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs atleast one letter';
+        }
+
+        if(preg_match('/.*\d+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs atleast one number';
+        }
+    }
+
 }
