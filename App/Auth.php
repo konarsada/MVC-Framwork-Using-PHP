cabbcd82b3993f777a71ca7d518c319457ab937c
konarsada <sadabratak@gmail.com>
2021-12-21 02:16:38 +0530

    Restrict access to authenticated users only

---
+++ App/Auth.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App;
+
+/**
+ * Authentication
+ */
+class Auth {
+    /**
+     * Login the user
+     * 
+     * @param User $user The user model
+     * 
+     * @return void
+     */
+    public static function login($user) {
+        session_regenerate_id(true);
+
+        $_SESSION['user_id'] = $user->id;
+    }
+
+    /**
+     * Logout the user
+     * 
+     * @return void
+     */
+    public static function logout() {
+        // Unset all of the session variables.
+        $_SESSION = [];
+
+        // If it's desired to kill the session, also delete the session cookie.
+        // Note: This will destroy the session, and not just the session data!
+        if(ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        // Finally, destroy the session.
+        session_destroy();
+    }
+
+    /**
+     * Return indicator of whether a user is logged in or not
+     * 
+     * @return boolean
+     */
+    public static function isLoggedIn() {
+        return isset($_SESSION['user_id']);
+    }
+
+    /**
+     * Remember the originally requested page in the session
+     * 
+     * @return void
+     */
+    public static function rememberRequestedPage() {
+        /* url = localhost/items/index
+        $_SERVER['REQUEST_URI'] = /items/index */
+        $_SESSION['return_to'] = $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * Get the originally requested page to return after requiring login, or default to the home page
+     * 
+     * @return void
+     */
+    public static function getReturnToPage() {
+        return $_SESSION['return_to'] ?? '/';
+    }
+}
---
+++ App/Controllers/Items.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+/**
+ * Items controller
+ */
+class Items extends \Core\Controller {
+    /**
+     * Items index
+     * 
+     * @return void
+     */
+    public function indexAction() {
+        $this->requireLogin();
+        
+        View::renderTemplate('Items/index.html');
+    }
+}
